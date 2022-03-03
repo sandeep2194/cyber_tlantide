ff863e303f0f8120ee592271ec6bfd6ce9d76b1d
sandeep2194 <sandeep2194@live.com>
2022-03-03 18:36:18 +0000

    added create csv file with name

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,10 @@ const billService = require('../services')
 
 exports.bill = async (req, res) => {
     try {
-        const { client, bill } = req.query
-        if (!client || !bill)
+        const { data } = req.query
+        if (!data)
             return res.status(400).send('Phone number required')
-        const save = billService.saveBill({ client, bill });
+        const save = billService.saveBill({ ...data });
         return res.status(200).send('bill saved')
     } catch (error) {
         res.status(500).send({ 'status': error })
--- server/services/index.js
+++ server/services/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const helpers = require('../utils/helpers');
+const CSV = require('csv-blink');
 
 exports.saveBill = (data) => {
     const today = helpers.todayDate();
@@ -12,20 +13,29 @@ exports.saveBill = (data) => {
 }
 
 const createCSV = (data, dir) => {
-    const ObjectsToCsv = require('objects-to-csv');
-    // Sample data - two columns, three rows:
-    const data2 = [
-        { code: 'CA', name: 'California' },
-        { code: 'TX', name: 'Texas' },
-        { code: 'NY', name: 'New York' },
-    ];
-    // If you use "await", code must be inside an asynchronous function:
-    (async () => {
-        const csv = new ObjectsToCsv(data2);
-        // Save to file:
-        await csv.toDisk(path.join(dir, data2[0].name + '.csv'));
-        // Return the CSV file as string:
-        console.log(await csv.toString());
-    })();
+    const csv = new CSV(
+        ["No Facture", "Nom Entreprise", "Nom", "Courriel", "Adresse", "Telephone", "# Cheque", "Livraison", "Sous Total", "TPS", "TVQ", "Grand Total", "produit", "options", "quantite", "prix"],
+    )
+    const row = Object.values(data);
+    csv.addRow(row)
+    const file = fs.writeFile(path.join(dir, data.noFacture + '.csv'), csv.file, (err) => helpers.logError(err))
+    console.log('done', csv.file);
 }
-this.saveBill();
+// this.saveBill({
+//     noFacture: "test1223",
+//     nf2: "test",
+//     nf3: "test",
+//     nf4: "test",
+//     nf5: "test",
+//     nf6: "test",
+//     nf7: "test",
+//     nf8: "test",
+//     nf9: "test",
+//     nf10: "test",
+//     nf11: "test",
+//     nf12: "test",
+//     nf13: "test",
+//     nf14: "test",
+//     nf15: "test",
+//     nf16: "test",
+// });
